830b0ce5b32699ab350fe5957820c8725b1a194f
Busra Kaya <bsr.kaya13@gmail.com>
2024-09-22 20:15:50 +0000

    template engine

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-'use strict'
+'use strict';
 const express = require('express');
-///const myDB = require('./connection');
-///const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const app = express();
-
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-//fccTesting(app); // For fCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  // Change the response to render the Pug template
   res.render('index');
 });
 const cors = require('cors');
